dbbffc2f54a98f34aac9a561157447500ff851f3
Pascal Aigner <pascal.aigner97@outlook.com>
2022-05-13 22:41:07 +0200

    added get club league games functionality

--- get_club_members.py
+++ get_club_members.py
@@ -2,7 +2,8 @@ from decouple import config
 import os
 import requests
 import pandas as pd
-import sqlalchemy as db
+from sqlalchemy import create_engine
+from sqlalchemy.pool import NullPool
 
 FIXIE_URL=config('FIXIE_URL')
 API_TOKEN=config('API_TOKEN')
@@ -10,7 +11,6 @@ URI=config('URI')
 
 club_tag = '#2YPY9LVV9'
 proxies = {
-    # https://devcenter.heroku.com/articles/fixie
     'http'  : os.environ.get('FIXIE_URL', ''),
     'https' : os.environ.get('FIXIE_URL', ''),
 }
@@ -27,23 +27,14 @@ response = requests.get(
 club_members_list = response.json()['items']
 club_members_df = pd.DataFrame(
     {
-        'player_tag'  : [club_members_list[i]['tag']  for i in range(len(club_members_list))],
-        'player_name' : [club_members_list[i]['name'] for i in range(len(club_members_list))],
+        'player_tag'  : [member['tag'] for member in club_members_list],
+        'player_name' : [member['name'] for member in club_members_list],
     },
 )
 
-# https://towardsdatascience.com/sqlalchemy-python-tutorial-79a577141a91
-engine = db.create_engine(URI)
-connection = engine.connect()
-metadata = db.MetaData()
-club_members_table = db.Table('club_members', metadata, autoload=True, autoload_with=engine)
+engine = create_engine(URI, poolclass=NullPool)
+with engine.connect() as connection:
+    connection.execute('TRUNCATE TABLE club_members')
+    club_members_df.to_sql('club_members', connection, if_exists='append', index=False)
 
-connection.execute('TRUNCATE TABLE club_members')
-query = db.insert(club_members_table)
-connection.execute(query, club_members_df.to_dict('records'))
-
-# https://stackoverflow.com/questions/21738944/how-to-close-a-sqlalchemy-session
-connection.close()
-engine.dispose()
-
-print("Script run successfully")
+print("get_club_members.py executed successfully")
---
+++ sql_scripts/create_club_league_games_table.sql
@@ -0,0 +1,4 @@
+CREATE TABLE club_league_games(
+    game_timestamp TEXT PRIMARY KEY,
+    game_json TEXT
+);

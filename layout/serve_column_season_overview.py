d2cfa02aba2391e92539a2aa152c1cad14f26282
Pascal Aigner <pascal.aigner97@outlook.com>
2022-05-21 12:02:52 +0200

    update CSS

--- layout/serve_column_season_overview.py
+++ layout/serve_column_season_overview.py
@@ -34,9 +34,9 @@ def serve_column_season_overview(club_members_df, club_league_games_df):
                             class_name='selector',
                             options=[
                                 {'label' : 'All', 'value' : 'all'},
-                                {'label' : 'Event day 1', 'value' : '1'},
-                                {'label' : 'Event day 2', 'value' : '2'},
-                                {'label' : 'Event day 3', 'value' : '3'},
+                                {'label' : '1', 'value' : '1'},
+                                {'label' : '2', 'value' : '2'},
+                                {'label' : '3', 'value' : '3'},
                             ],
                             value='all',
                         ),
